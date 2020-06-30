700f512dc64b9cee8319fce2a375ac9bec3beb00
pduren <57735971+kParkz@users.noreply.github.com>
2020-06-30 11:51:03 -0500

    add updateItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -12,14 +12,22 @@ const Mutations = {
     );
     return item;
   },
-
-  //   createDog(parent, args, ctx, info) {
-  //     global.dogs = global.dogs || [];
-  //     // create a dog!
-  //     const newDog = { name: args.name };
-  //     global.dogs.push(newDog);
-  //     console.log(args);
-  //   },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove the ID from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
