1fd868baf6b84af31c83251f753fb511c849d318
pduren <57735971+kParkz@users.noreply.github.com>
2020-06-30 11:51:27 -0500

    UpdateItem & item init

---
+++ sick-fits/frontend/pages/update.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UpdateItem from "../components/UpdateItem";
+
+const Sell = ({ query }) => (
+  <div>
+    <UpdateItem id={query.id} />
+  </div>
+);
+
+export default Sell;
