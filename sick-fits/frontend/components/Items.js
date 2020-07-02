7a1b31413ea44e374c8bad143506c15dc38094b5
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-02 10:53:12 -0500

    all items query

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,10 +3,14 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
 import Item from "./Item";
+import Pagination from "./Pagination";
+import { perPage } from "../config";
 
-export const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+// had to get rid of orderBy: createdAt_DESC in the query (comes after skip: $skip) for it to work correctly for some reason 0_0
+
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip) {
       id
       title
       price
@@ -28,12 +32,18 @@ const ItemsList = styled.div`
   margin: 0 auto;
 `;
 
-export default class Items extends Component {
+class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          // fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage,
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
@@ -46,7 +56,11 @@ export default class Items extends Component {
             );
           }}
         </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
 }
+
+export default Items;
+export { ALL_ITEMS_QUERY };
