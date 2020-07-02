d4fb7988da8b473be1a5e8a9edb4f54aaeb52fb9
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-02 14:14:23 -0500

    backend for signin, signout, signup

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -70,6 +70,31 @@ const Mutations = {
     // finally, we return the user to the browser
     return user;
   },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. check if user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for ${email}`);
+    }
+    //2 . check if password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid Password");
+    }
+    // 3. generate jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //4 . set the cookie with the token
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    // 5. return the user
+    return user;
+  },
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie("token");
+    return { message: "Goodbye!" };
+  },
 };
 
 module.exports = Mutations;
