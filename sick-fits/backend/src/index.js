3dd10e8087080e28b2d1d11ff2601b12c19fbb26
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-02 10:52:44 -0500

    backend changes

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,9 +1,10 @@
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
 const server = createServer();
 
-// TODO Use express middleware to handle cookies
+server.express.use(cookieParser());
 // TODO Use express middleware to populate current user
 
 server.start(
