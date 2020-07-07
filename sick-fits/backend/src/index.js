6db7315fc742c10ea8ca6471ce177279d5f93916
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-07 11:12:58 -0500

    fix prisma permissions

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,20 +1,35 @@
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
+
 const server = createServer();
 
 server.express.use(cookieParser());
 
-// decode the jwt so we can get the user id on each request
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    // put userId onto the req for future requests to access
+    // put the userId onto the req for future requests to access
     req.userId = userId;
   }
+  next();
+});
+
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{ id, permissions, email, name }"
+  );
+  req.user = user;
   next();
 });
 
@@ -26,6 +41,6 @@ server.start(
     },
   },
   (deets) => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,11 +1,12 @@
 const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 
 const Query = {
   items: forwardTo("db"),
   item: forwardTo("db"),
   itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
-    // check if there is a current userId
+    // check if there is a current user ID
     if (!ctx.request.userId) {
       return null;
     }
@@ -16,10 +17,18 @@ const Query = {
       info
     );
   },
-  //   async items(parent, args, ctx, info) {
-  //     const items = await ctx.db.query.items();
-  //     return items;
-  //   },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    console.log(ctx.request.userId);
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+
+    // 2. if they do, query all the users!
+    return ctx.db.query.users({}, info);
+  },
 };
 
 module.exports = Query;
